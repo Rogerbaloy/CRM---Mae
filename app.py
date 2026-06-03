e52d371df2fc14cf22c18c4b461cba9ecc14beeb
Roger <68353608+Rogerbaloy@users.noreply.github.com>
2026-06-03 09:16:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -179,8 +179,8 @@ with aba3:
                     st.rerun()
 
            # --- BLOCO 4: CADASTRO CORRIGIDO ---
-               if st.form_submit_button("Cadastrar Produto"):
-               if nome_prod and marca:
+                if st.form_submit_button("Cadastrar Produto"):
+                if nome_prod and marca:
                    codigos = [int(x) for x in df_atualizado['Codigo'].tolist() if str(x).isdigit()]
                    novo_codigo = max(codigos) + 1 if codigos else 1
         
